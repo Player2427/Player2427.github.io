de8748be0842f13f91645d0a70f6fff62820c4f0
Player2427 <74377043+Player2427@users.noreply.github.com>
2022-03-09 12:24:14 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+﻿
+
+	
+	var login = document.getElementById('login');
+	var reg = document.getElementById('reg');
+	
+	var login_content = document.getElementById('login_content');
+	var reg_content = document.getElementById('reg_content');
+
+	reg.onclick = function() {
+		
+		login_content.style = "display: none";
+		reg_content.style = "display: block";
+		
+		login.style = "background-color: #EDEDED; color: #000;";
+		reg.style = "background-color: #FF0044; color: #fff;";
+					
+	};
+	
+	login.onclick = function() {
+
+		reg_content.style = "display: none";
+		login_content.style = "display: block";
+		
+		reg.style = "background-color: #EDEDED; color: #000;";
+		login.style = "background-color: #FF0044; color: #fff;";
+		
+	};
